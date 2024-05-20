b114dac08637e2d28ad5711b9f3e023884d75dcb
ABugNamedJune <145883736+ABugNamedJune@users.noreply.github.com>
2024-05-20 15:54:53 -0700

    Add files via upload

---
+++ Utilities/primary_ui.py
@@ -0,0 +1,4 @@
+#PyPlayer - June Bush - June@JuneBush.com - GPL3.0 - Last updated 5.20.24
+#This is the main UI element, it's hard for me to descripe what this does succinctly in the header of this file
+#but suffice it to say this organizes data into a list that can be displayed on screen and interacted with
+#... but at the same time I haven't written fucking any of this, so right now it does nothing.
