32d7f7aa2d80fa64ba5031dbf2d5b4661d2c21a6
ABugNamedJune <145883736+ABugNamedJune@users.noreply.github.com>
2024-05-20 15:55:23 -0700

    Delete Utilities/import json.py

--- Utilities/import json.py
+++
@@ -1,9 +0,0 @@
-import json
-
-blah = []
-
-for i in range(1, 1000):
-    blah.append({"title": "average title", "artist": "artist name", "album": "album name"})
-
-with open('test.json', 'w') as fout:
-    json.dump(blah, fout)
